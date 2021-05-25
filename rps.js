da1843222ed97b168bbc01ff4c244b94cb5de74e
Bryan <bryan.how@live.com>
2021-05-25 23:22:59 +0800

    add playRound function

--- rps.js
+++ rps.js
@@ -14,3 +14,31 @@ function computerPlay() {
 //console.log(computerPlay());
 //console.log(computerPlay());
 //console.log(computerPlay());
+
+// a function that plays a single round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return "It's a tie!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats rock!";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "It's a tie!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beat paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "It's a tie!";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beat paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors!";
+    }
+}
+
+// for testing the playRound function
+//let playerSelection = "scissors";
+//let computerSelection = "paper";
+//console.log(playRound(playerSelection, computerSelection));
